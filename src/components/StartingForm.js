680b52fc20f369e6394c5bd529c10c8933e285ff
Kenneth Lyerly <kenput3r@gmail.com>
2019-11-16 18:23:41 -0800

    support for progress updates

--- src/components/StartingForm.js
+++ src/components/StartingForm.js
@@ -9,6 +9,10 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import GetInventory from '../utils/GetInventory';
+import GetBins from '../utils/GetBins';
+import GetTransferList from '../utils/GetTransferList';
+import DearLogo from '../DearLogo.png';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -21,6 +25,18 @@ const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
   },
+  progressBar: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+  progressMessage: {
+    fontWeight: 'normal', 
+    marginBlockStart: '2em', 
+    marginBlockEnd: '2.5em', 
+    textTransform: 'uppercase'
+  },
 }));
 
 function StartingForm(props) {
@@ -30,12 +46,6 @@ function StartingForm(props) {
     to: 'Apparel',
   });
 
-  const inputLabel = React.useRef(null);
-  const [labelWidth, setLabelWidth] = React.useState(0);
-  // React.useEffect(() => {
-  //   setLabelWidth(inputLabel.current.offsetWidth);
-  // }, []);
-
   const handleChange = name => event => {
     setState({
       ...state,
@@ -43,16 +53,48 @@ function StartingForm(props) {
     });
   };
 
-  const handleStart = event => {
-    props.setView('TranferForm');
-    GetInventory();
+  const [progressMessage, setProgressMessage] = React.useState('CLICK START TO BUILD THE TRANSFER LIST');
+  const [completed, setCompleted] = React.useState(0);
+
+  const updateProgress = (message, complete) => {
+    console.log(message);
+    setProgressMessage(message);
+    setCompleted(complete);
+  }
+
+  const handleStart = async event => {
+    const bins = await new Promise((res, rej) => {
+      updateProgress('getting bins list', 3)
+      GetBins({resolve: res, reject: rej});
+    }).catch(error => {
+      console.log(error)
+    });
+    if(!bins) {
+      console.log('something went wrong getting the bins');
+      return;
+    }
+    const inventory = await new Promise((res, rej) => {
+      GetInventory(1, updateProgress, {resolve: res, reject: rej})
+    }).catch(error => {
+      console.log(error);
+    });
+    if(!inventory) {
+      console.log('something went wrong getting the inventory');
+      return;
+    }
+    const list = GetTransferList(state.from, state.to, inventory);
+    console.log(list);
+    props.setRows(list);
   }
 
   return (
     <Box>
       <Container>
         <Grid container spacing={1} align="center" justify="center" direction="column" style={{height: '100vh'}}>
-          <Grid container spacing={0} direction="row">
+          <Grid container spacing={0} direction="row" justify="center">
+            <Grid container item xs={12} justify="center">
+              <h1 style={{color: '#747F8C', marginBottom: '100px'}}><img src={DearLogo} alt="Dear Systems Logo" /> bin_stocker_v2</h1>
+            </Grid>
             <Grid container item xs={6} spacing={3} justify="center">
               <FormControl className={classes.formControl}>
                 <InputLabel htmlFor="from-native-helper">Pick From</InputLabel>
@@ -95,6 +137,12 @@ function StartingForm(props) {
               <Button variant="outlined" color="primary" className={classes.button} onClick={handleStart}>
                 Start
               </Button>
+            </Grid>
+            <Grid container item xs={6} justify="center">
+              <h5 className={classes.progressMessage}>{progressMessage}</h5>
+              <div className={classes.progressBar}>
+                <LinearProgress variant="determinate" value={completed} />
+              </div>
             </Grid>
           </Grid>
         </Grid>
