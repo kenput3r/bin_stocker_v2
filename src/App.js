424378ddacce27355b41f5006fcefe10f96d1fd9
Kenneth Lyerly <kenput3r@gmail.com>
2019-11-16 18:24:46 -0800

    change views after table data is ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import StartingForm from './components/StartingForm';
 import TransferForm from './components/TransferForm';
@@ -6,6 +6,7 @@ import TransferForm from './components/TransferForm';
 function App() {
   const [state, setState] = React.useState({
     view: 'StartingForm',
+    rows: [],
   });
   
   const setView = view => setState({
@@ -13,33 +14,23 @@ function App() {
     view: view,
   });
 
-  let index = 0;
-  function createData(qty, sku, name, from_bin, to_bin) {
-    index += 1
-    return { qty, sku, name, from_bin, to_bin, id: index };
-  }
+  const setRows = rows => setState({
+    ...state,
+    rows: rows,
+  });
 
-  const rows = [
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Donut', 452, 25.0, 51, 4.9),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Honeycomb', 408, 3.2, 87, 6.5),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Jelly Bean', 375, 0.0, 94, 0.0),
-    createData('KitKat', 518, 26.0, 65, 7.0),
-    createData('Lollipop', 392, 0.2, 98, 0.0),
-    createData('Marshmallow', 318, 0, 81, 2.0),
-    createData('Nougat', 360, 19.0, 9, 37.0),
-    createData('Oreo', 437, 18.0, 63, 4.0),
-  ];
+  useEffect(() => {
+    if(state.rows.length) {
+      setView('TransferForm');
+    }
+  },[state.rows]);
+
   return (
     <div className="App">
       {state.view === 'StartingForm' ?
-      <StartingForm setView={setView} />
+      <StartingForm setRows={setRows} />
       :
-      <TransferForm rows={rows} />}
+      <TransferForm rows={state.rows} />}
     </div>
   );
 }
