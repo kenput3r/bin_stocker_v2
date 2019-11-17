35b2a8c545e60fee23fdbcf6cae8e7e0f5a99aed
Kenneth Lyerly <kenput3r@gmail.com>
2019-11-17 12:47:39 -0800

    support for transfers from Townsend, support for stranded inventory

--- src/utils/GetTransferList.js
+++ src/utils/GetTransferList.js
@@ -7,27 +7,51 @@ const GetTransferList = (from_location, to_location, inventory_list) => {
     return { qty, sku, name, from_bin, to_bin, id: index };
   }
   let list = [];
+  let homeless_items = [];
   const warehouse = from_location === 'Townsend' ? 'Townsend' : 'Main Warehouse';
   const keys = Object.keys(inventory_list);
+  //iterate over each item in the inventory list
   for(let i = 0; i < keys.length; i++) {
     const key = keys[i];
     const sku = inventory_list[key].sku;
     const name = inventory_list[key].name;
     const bin_max = GetBinMax(sku, name, to_location);
-    //const bins = inventory_list[key].locations[warehouse] || [];
     let bins = [];
-    if(inventory_list[key].locations[warehouse] && inventory_list[key].locations[warehouse].bins) {
+    if(warehouse === 'Townsend') {
+      const location_keys = Object.keys(inventory_list[key].locations)
+      for(const location_key of location_keys) {
+        let location = inventory_list[key].locations[location_key];
+        if(location.name !== 'Retail Store' && location.bins.length) {
+          bins = bins.concat(location.bins);
+        }
+      }
+    }else if(inventory_list[key].locations[warehouse] && inventory_list[key].locations[warehouse].bins){
       bins = inventory_list[key].locations[warehouse].bins;
     }
     let from_bins = [];
     let to_bins = [];
     for(const bin of bins) {
-      if(bin.name.includes(from_location)) {
-        from_bins.push(bin);
-      }else if(bin.name.includes(to_location)) {
-        to_bins.push(bin);
+      //handle from location being Townsend
+      if(warehouse === 'Townsend') {
+        //if bin name doesn't include a picking prefix, it must be a Townsend bin
+        if(!bin.name.includes('Apparel') 
+        && !bin.name.includes('Merch') 
+        && !bin.name.includes('Pallet Racks') 
+        && !bin.name.includes('Picking')) {
+          from_bins.push(bin);
+        }else if(bin.name.includes(to_location)) {
+          to_bins.push(bin);
+        }
+      //handle from location not being Townsend
+      }else{
+        if(bin.name.includes(from_location)) {
+          from_bins.push(bin);
+        }else if(bin.name.includes(to_location)) {
+          to_bins.push(bin);
+        }
       }
     }
+    //if we have both a picking and put away location
     if(from_bins.length && to_bins.length) {
       for(const bin of to_bins) {
         //if bin is at 50% or less capacity
@@ -58,8 +82,64 @@ const GetTransferList = (from_location, to_location, inventory_list) => {
           }
         }
       }
+    //check for products that don't have a put away location
+    }else if(from_bins.length && !to_bins.length){
+      for(const from_bin of from_bins) {
+        const char1 = sku.charAt(0).toLowerCase();
+        const char2 = sku.charAt(1);
+        let qty_needed = bin_max;
+        let qty = 0;
+        if(qty_needed > from_bin.available) {
+          qty = from_bin.available;
+          from_bin.available = 0;
+        }else{
+          qty = qty_needed;
+          from_bin.available -= qty_needed;
+        }
+        //handle putting away apparel
+        if(to_location.includes('Apparel')) {
+          if(char1 === 's' || char1 === 'h') {
+            homeless_items.push(
+              createData(
+                qty,
+                sku,
+                name,
+                from_bin.name,
+                '?'
+              )
+            );
+          }
+        //handle putting away merch
+        }else if(to_location.includes('Merch')) {
+          if(char1 === 'm' || char1 === 'c') {
+            homeless_items.push(
+              createData(
+                qty,
+                sku,
+                name,
+                from_bin.name,
+                '?'
+              )
+            );
+          }
+        //handle putting away product
+        }else if(to_location.includes('Picking')) {
+          if(char1 === 'p') {
+            homeless_items.push(
+              createData(
+                qty,
+                sku,
+                name,
+                from_bin.name,
+                '?'
+              )
+            );
+          }
+        }
+      }
     }
   }
+  if(homeless_items.length) list = list.concat(homeless_items);
   return list;
 }
 
